aa755de98f0b5fefeec9242cffbb078e04529756
Jagjit <31589310+JagjitBhatia-UIC@users.noreply.github.com>
2019-06-27 12:02:02 -0500

    Add initial app.js containing TaskManager object
    Task Manager Object contains functions createTask(), updateTask(), deleteTask(), and getAllTasks()

---
+++ app.js
@@ -0,0 +1,68 @@
+const db = require('./taskdb.js');
+
+exports.createTask = function (newTask, response) {
+	let name = newTask.name;
+	let description = newTask.description;
+	let time = newTask.time;
+	let completed = 0;
+	
+	db.query(`INSERT INTO Tasks (name, description, time, completed) VALUES ("${name}", "${description}", "${time}", ${completed})`, function(err, res) {
+		if(err) {
+			console.log("error: ", err);
+			response(err, null);
+		}
+
+		else {
+			response(null, res);
+		}
+	});
+};
+
+exports.updateTask = function (updatedTask, response) {
+	let id = updatedTask.id;
+	let name = updatedTask.name;
+	let description = updatedTask.description;
+	let time = updatedTask.time;
+	let completed = updatedTask.completed;
+
+	db.query(`UPDATE Tasks SET name = "${name}", description = "${description}, time = "${time}, completed = ${completed} WHERE id = ${id}`, function(err, res) {
+		if(err) {
+			console.log("error ", err);
+			response(err, null);
+		}
+
+		else {
+			response(null, res);
+		}
+	});
+};
+
+exports.deleteTask = function (id, response) {
+	db.query(`DELETE FROM Tasks WHERE id = ${id}`, function(err, res) {
+		if(err) {
+			console.log("error ", err);
+			response(err, null);
+		}
+
+		else {
+			response(null, res);
+		}
+	})
+};
+
+
+exports.getAllTasks = function (id, response) {
+	db.query(`SELECT * FROM Tasks`, function(err, res) {
+		if(err) {
+			console.log("error ", err);
+			response(err, null);
+		}
+
+		else {
+			response(null, res);
+		}
+	})
+};
+
+
+
